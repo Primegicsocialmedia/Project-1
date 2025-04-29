f72d45b32fb9698d953574e123015143ce26b944
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-04-29 18:11:44 +0200

    loading screen done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import SignInSignUp from "./components/SignInSignUp";
+import { useState } from "react";
+import LoadingScreen from "./components/LoadingScreen";
+import Navbar from "./components/Navbar";
+
 
 
 
 const App = () => {
+
+const [isLoaded, setIsLoaded] = useState(false)
+
   return (
-    <div >
-   <SignInSignUp />
+   <>
+   {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+    <div  className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}>
+          <Navbar/>
+
     </div>
+   </>
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+        <div className="max-w-5xl mx-auto px-4">
+            <div className="flex justify-between items-center h-16">
+            <a href="#home" className="font-mono text-xl font-bold text-white">
+            {" "}
+            Prime<span className="text-blue-500"> Tech</span>{" "}
+          </a>
+
+            </div>
+
+        </div>
+      
+    </nav>
+  );
+};
+
+export default Navbar;
