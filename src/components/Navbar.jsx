d26559db770e8df264e3576a335e477ee5e7ffe9
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-04-29 11:20:15 +0200

    3rd

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="bg-deepblue container">
+    <h1 className="text-5xl font-bold text-white">
+      This is the template with TailwindCSS
+    </h1>
+    <button className="bg-amber text-alabaster text-2xl font-bold mt-12 rounded px-6 py-4 animate-pulse">
+      Click here !
+    </button>
+    <input
+      type="text"
+      placeholder="Enter your name"
+      className="bg-white text-lg text-amber border-2 rounded ml-4 outline-0"
+    />
+  </div>
+  )
+}
+
+export default Navbar
