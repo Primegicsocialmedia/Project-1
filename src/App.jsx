b247882ca4f0081b3128c133067b7be9ca27060d
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-04-30 12:45:55 +0200

    contact form completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MobileMenu from "./components/MobileMenu";
 import Home from "./components/sections/Home";
 import About from "./components/sections/About";
 import Projects from "./components/sections/Projects";
+import { Contact } from "./components/sections/Contact";
 
 
 
@@ -25,6 +26,7 @@ const [menuOpen, setMenuOpen] = useState(false)
           <Home />
           <About />
           <Projects />
+          <Contact />
     </div>
    </>
   );
