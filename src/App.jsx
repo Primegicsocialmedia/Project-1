8ce6755fcc8f35bac2a2f3b9bb2f071ccda64015
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-04-30 11:27:02 +0200

    Home page done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import LoadingScreen from "./components/LoadingScreen";
 import Navbar from "./components/Navbar";
 import MobileMenu from "./components/MobileMenu";
+import Home from "./components/sections/Home";
 
 
 
@@ -19,7 +20,7 @@ const [menuOpen, setMenuOpen] = useState(false)
         } bg-black text-gray-100`}>
           <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
           <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-
+          <Home />
     </div>
    </>
   );
