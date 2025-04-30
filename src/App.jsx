ce9370d75d564926860a63b8932d5ba706d1db82
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-04-30 11:53:02 +0200

    about done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LoadingScreen from "./components/LoadingScreen";
 import Navbar from "./components/Navbar";
 import MobileMenu from "./components/MobileMenu";
 import Home from "./components/sections/Home";
+import About from "./components/sections/About";
 
 
 
@@ -21,6 +22,7 @@ const [menuOpen, setMenuOpen] = useState(false)
           <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
           <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
           <Home />
+          <About />
     </div>
    </>
   );
