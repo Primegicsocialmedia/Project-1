6232d597f83ab5038545795c0b9b7b4af0484b91
Primegicsocialmedia <valentio.samuels777@gmail.com>
2025-04-30 11:13:44 +0200

    mobile menu complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import LoadingScreen from "./components/LoadingScreen";
 import Navbar from "./components/Navbar";
+import MobileMenu from "./components/MobileMenu";
 
 
 
@@ -8,6 +9,7 @@ import Navbar from "./components/Navbar";
 const App = () => {
 
 const [isLoaded, setIsLoaded] = useState(false)
+const [menuOpen, setMenuOpen] = useState(false)
 
   return (
    <>
@@ -15,7 +17,8 @@ const [isLoaded, setIsLoaded] = useState(false)
     <div  className={`min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}>
-          <Navbar/>
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
 
     </div>
    </>
